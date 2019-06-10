6abf80e13070973af7abe3c88865cde8fef075bf
Bobby Martin <40462825+BobbyBleacher@users.noreply.github.com>
2019-06-10 10:39:09 -0500

    Bug Fixes

---
+++ vitals.php
@@ -0,0 +1,16 @@
+<?php
+    
+ //write file being sent to vitals.json
+ $upload_file = 'vitals.json'; 
+ $handle = fopen($upload_file,'w'); 
+
+//Make sure there is actually data
+if ('' == @file_get_contents('php://input'))
+{
+ echo "No JSON received, empty file";
+} else{
+ fwrite($handle, @file_get_contents('php://input')); 
+ fclose($handle);
+}
+
+ ?>
